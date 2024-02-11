c639a269c495b0e4e29f7b828ef1cfb51b9d3e31
kan5470 <monkeyboyer.ks@gmail.com>
2024-02-11 23:43:59 +0800

    update

---
+++ color/1grey/ABC049-B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <iterator>  // std::back_inserter()
+#include <set>
+#include <map>
+#include <algorithm>  // std::copy()
+#include <functional>  // std::greater<T>()
+#include <utility>  // std::swap()
+#include <numeric>  // accumulate(ALL(vec), 0)  0 は初期値
+#include <cmath>
+#include <climits>  // INT_MIN
+#include <cctype>  // std::isdigit()
+#include <iomanip>  // std::setprecision()
+using namespace std;
+
+#define ALL(obj) (obj).begin(), (obj).end()
+#define REP(i, n) for (int i=0; i < (int)(n); i++)  // 0 ~ n-1
+#define REPN(i, n) for (int i=1; i <= (int)(n); i++)  // 1 ~ n
+#define MIN(vec) min_element(ALL((vec)))  // イテレータのため、値を取り出すときは * を先頭につける
+#define MAX(vec) max_element(ALL((vec)))
+#define IDX(vec, element_iter) distance((vec).begin(), element_iter)
+#define SUM(vec) accumulate(ALL((vec)), 0)  // 0 は初期値
+#define COPY(vec1, vec2) copy(ALL(vec1), back_inserter(vec2))  // vec1をvec2にコピーする vec2は空にしておく必要あり
+typedef long long ll;
+
+const int MOD = 1000000007;  // 1 000 000 007
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+    vector<vector<char>> vec(h, vector<char> (w));
+    REP(i, h) {
+        REP(j, w) {
+            cin >> vec[i][j];
+        }
+    }
+
+    REP(i, h) {
+        REP(j, w) {
+            cout << vec[i][j];
+        }
+        cout << endl;
+        REP(j, w) {
+            cout << vec[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
